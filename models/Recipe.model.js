074e853d478e1029e6adfee92761e69c7601ed8d
sortros <albiol957@hotmail.com>
2021-04-26 13:42:39 +0200

    wip

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,12 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title:"",
+  level:["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  ingredients:[""],
+  cuisine:"",
+  
+
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
