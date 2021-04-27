60ef92720c72e1c037ebf042ba7210c8b4f73b1c
sortros <albiol957@hotmail.com>
2021-04-27 13:15:59 +0200

    wip

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,15 +3,15 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
-  title:{ type: String },
+  title:{ type: String , unique: true, required: true},
   level:{ type: String },
   ingredients:[String],
-  cuisine: { type: String },,
-  dishType:{ type: String },,
+  cuisine: { type: String , required:true},
+  dishType:{ type: String },
   image: { type: String , default: 'https://images.media-allrecipes.com/images/75131.jpg'},
-  duration: { type: Number },,
+  duration: { type: Number , min: 0 },
   creator: { type: String },
-  created: { type: Date , default: "today"},;
+  created: { type: Date , default: "today"};
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
